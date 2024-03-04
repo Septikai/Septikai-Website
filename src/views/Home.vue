357b71f2e860aa983bea8d327ba29c6bb31061bd
Natalia Spence <52349324+Septikai@users.noreply.github.com>
2024-03-04 22:15:29 +0000

    update home page a little

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,11 @@
     <br/>
     <p>Welcome to my website!</p>
     <div v-if="isPersonal">
-      <p>I'm Natalia Spence, and here you can find some information about various projects I've worked on along with ways to get in contact with me.</p>
+      <p>I'm Natalia Spence, and here you can find some information about me, various projects I've worked on, and ways to get in contact with me.</p>
     </div>
     <div v-else>
-      <p>I made this because I got bored</p>
+      <p>I made this because I got bored.</p>
+      <p>Here you can find some information about me, various projects I've worked on, and ways to get in contact with me.</p>
     </div>
   </div>
 </template>
